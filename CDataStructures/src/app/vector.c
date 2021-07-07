f7a4924a8e4002f851d4c646795069e448996bb1
RenoirTan <renoirtan2005@gmail.com>
2021-07-07 22:34:05 +0800

    vector: test remove

--- CDataStructures/src/app/vector.c
+++ CDataStructures/src/app/vector.c
@@ -14,13 +14,33 @@ int main(int argc, char **argv) {
 
     printf("Vector allocated!\n");
     
-    for (int32_t i = 0xdead0000; i < 0xdead0100; i += 1) {
+    for (int32_t i = 0xdead0000; i < 0xdead0020; i += 1) {
         if (CDS_IS_ERROR(cds_vector_push_back(vector, &i))) {
             printf("Could not add numbers.\n");
+            goto errored;
         }
     }
 
     printf("Length of vector: %lu\n", vector->length);
+
+    printf("Remove all even numbers.\n");
+
+    {
+        size_t index = 0;
+        while (index < vector->length) {
+            int32_t *ptr = cds_vector_get(vector, index);
+            if (!(*ptr % 2)) {
+                if (CDS_IS_ERROR(cds_vector_remove(vector, index, ptr))) {
+                    printf("Could not remove numbers.\n");
+                    goto errored;
+                }
+            }
+            ++index;
+        }
+    }
+
+    printf("Length of vector: %lu\n", vector->length);
+    printf("Memory allocated: %lu\n", vector->_bytes_allocated);
 
     for (size_t index = 0; index < vector->length; index++) {
         int32_t number = *(int32_t*) cds_vector_get(vector, index);
